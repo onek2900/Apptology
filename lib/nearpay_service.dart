fdc2e3cff60464c7fc9e0107224ea278159db6f8
MN <m.nabil@apptologyinc.com>
2024-10-25 01:17:42 +0300

    v1.8 - Nearpay integrated

---
+++ lib/nearpay_service.dart
@@ -0,0 +1,61 @@
+import 'package:nearpay_flutter_sdk/nearpay.dart';
+import 'package:nearpay_flutter_sdk/errors/purchase_error/purchase_error.dart';
+import 'package:nearpay_flutter_sdk/util/util.dart';
+
+class NearpayService {
+  late Nearpay nearpay;
+
+  // Constructor to initialize NearpayService with authentication details
+  NearpayService() {
+    nearpay = Nearpay(
+      authType: AuthenticationType.login,
+      authValue: " ",
+      env: Environments.sandbox,
+      locale: Locale.localeDefault,
+    );
+  }
+
+  // Method to initialize Nearpay
+  Future<void> initializeNearpay() async {
+    try {
+      await nearpay.initialize();
+      await nearpay.setup();
+    } catch (e) {
+      throw "Initialization failed: $e";
+    }
+  }
+
+  // Method to logout from Nearpay
+  Future<void> logoutNearpay() async {
+    try {
+      await nearpay.logout();
+    } catch (e) {
+      throw "Logout failed: $e";
+    }
+  }
+
+  // Method to perform a purchase
+  Future<void> makePurchase({
+    required int amount,
+    required String customerReferenceNumber,
+    bool enableReceiptUi = true,
+    bool enableReversalUi = true,
+    bool enableUiDismiss = true,
+    int finishTimeout = 60,
+  }) async {
+    try {
+      await nearpay.initialize();
+      await nearpay.purchase(
+        amount: amount,
+        transactionId: '94fa0134-df2d-4006-8159-e4d66f82b38b',
+        customerReferenceNumber: customerReferenceNumber,
+        enableReceiptUi: enableReceiptUi,
+        enableReversalUi: enableReversalUi,
+        enableUiDismiss: enableUiDismiss,
+        finishTimeout: finishTimeout,
+      );
+    } catch(response) {
+      print('error $response');
+    }
+  }
+}
