c50c3072c559ca1735b77b0ee6203eb7d8d07446
MN <m.nabil@apptologyinc.com>
2024-10-31 10:36:02 +0300

    v2.0.1 - update on design

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,33 +1,44 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
+  // Define your primary color and other theme settings here
   static final ThemeData appTheme = ThemeData(
-    brightness: Brightness.dark, // Set to dark theme
-    primaryColor: Colors.green, // Primary color for app elements
-    colorScheme: ColorScheme.dark(primary: Colors.green), // Use ColorScheme for the dark theme
-    scaffoldBackgroundColor: Colors.grey[900], // Dark gray background color
+    brightness: Brightness.light,
+    primaryColor: Colors.green,
+    colorScheme: ColorScheme.light(primary: Colors.green),
+    scaffoldBackgroundColor: Colors.white,
     appBarTheme: AppBarTheme(
-      backgroundColor: Colors.grey[800], // AppBar background color
-      titleTextStyle: TextStyle(color: Colors.green, fontSize: 20), // AppBar title text style
+      backgroundColor: Colors.white,
+      titleTextStyle: TextStyle(color: Colors.green, fontSize: 20),
     ),
     buttonTheme: ButtonThemeData(
-      buttonColor: Colors.green, // Button background color
-      textTheme: ButtonTextTheme.primary, // Button text color
+      buttonColor: Colors.green,
+      textTheme: ButtonTextTheme.primary,
     ),
     textTheme: TextTheme(
-      displayLarge: TextStyle(color: Colors.white), // Use displayLarge for main text
-      displayMedium: TextStyle(color: Colors.white), // Use displayMedium for secondary text
-      bodyLarge: TextStyle(color: Colors.white), // Use bodyLarge for regular text
-      bodyMedium: TextStyle(color: Colors.white), // Use bodyMedium for regular text
+      bodyLarge: TextStyle(color: Colors.green), // Example of bodyText1 usage
+      bodyMedium: TextStyle(color: Colors.green), // Example of bodyText2 usage
     ),
     inputDecorationTheme: InputDecorationTheme(
       filled: true,
-      fillColor: Colors.grey[800], // Input field background color
+      fillColor: Colors.white,
       border: OutlineInputBorder(
-        borderSide: BorderSide(color: Colors.green), // Input field border color
+        borderSide: BorderSide(color: Colors.green),
       ),
-      labelStyle: TextStyle(color: Colors.green), // Label text color
-      hintStyle: TextStyle(color: Colors.grey[400]), // Hint text color
+      labelStyle: TextStyle(color: Colors.green),
+      hintStyle: TextStyle(color: Colors.grey[400]),
     ),
+  );
+
+  // Define introTitleStyle and introSubtitleStyle here
+  static final TextStyle introTitleStyle = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: Colors.green,
+  );
+
+  static final TextStyle introSubtitleStyle = TextStyle(
+    fontSize: 18,
+    color: Colors.green,
   );
 }
